dd1527ab6700849b8d218b805602c6001d5651d9
HAM <71740147+SeunghoHam@users.noreply.github.com>
2025-09-16 21:17:51 +0900

    Variant 삭제
     - 슈팅 및 기존 템플릿 삭제

--- YG/Source/YG/DataAssets/GirlsData.h
+++ YG/Source/YG/DataAssets/GirlsData.h
@@ -35,11 +35,21 @@ struct FHeroineStatic
 	EWeaponType AttackType = EWeaponType::Melee;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Heroine)
-	uint8 Heart =0;
-
+	uint8 Friendly =0; // 친밀도. 용사 - 플레이어의 관계
+	// 용사가 플레이어의 선택에 거부하지 않게 됨
+	 
+	 // 상승 : 이벤트, 의뢰 성공
+	 // 하락 : 먼가 잘못된 선택을 했을 때, 의뢰 실패
+	 
+	 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Heroine)
-	uint8 Strange =0;
-
+	uint8 Relationship =0; // 우정도. 용사 - 플레이어의 신뢰
+	// 우정도가 낮아지면 탈주 가능성이 생김
+		
+	// 상승 : 임무성공
+	// 하락 : 임무실패
+	
+	
 	// 초기화에 사용할 기본 스탯(예: Affection, Trust 등)
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Heroine")
 	TMap<FName, int32> DefaultStats; // 이름 변경하게되면 HeroineSystem에서도 적용 변경해주기
